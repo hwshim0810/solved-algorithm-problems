82b06922013cf080b385466a397ff30ebc918f95
Shim.hw <shim.hw0810@gmail.com>
2017-04-08 12:06:45 +0900

    2017-google-codejam Qualification_B small case

--- src/codejam/P_2017_QB.java
+++ src/codejam/P_2017_QB.java
@@ -54,8 +54,8 @@ Note that the last sample case would not appear in the Small dataset.
 
 public class P_2017_QB {
 	public static void main(String[] args) throws InterruptedException, IOException {
-        BufferedReader br = new BufferedReader(new FileReader("B-small-attempt0.in"));
-        FileOutputStream output = new FileOutputStream("small_output.out");
+        BufferedReader br = new BufferedReader(new FileReader("QB_small.in"));
+        FileOutputStream output = new FileOutputStream("QB_small.out");
         
         while(true) {
             String line = br.readLine();
@@ -64,7 +64,7 @@ public class P_2017_QB {
             int T = Integer.valueOf(line);
             
             for (int i = 1; i <= T; ++i) {
-            	String res = String.format("Case #%d: %s%n", i, eval(br.readLine()));
+            	String res = String.format("Case #%d: %s%n", i, sm_eval(br.readLine()));
             	output.write(res.getBytes());
             }
             output.close();
@@ -72,7 +72,8 @@ public class P_2017_QB {
         br.close();
 	}
 	
-	public static String eval(String input) {
+	// Small
+	public static String sm_eval(String input) {
 		long N = Long.valueOf(input);
 		
 		if (N < 10) {
